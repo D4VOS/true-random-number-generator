3932edb87a5a2aa98b2b61019db19ad470c1d420
D4VOS <dawid.bryla@student.put.poznan.pl>
2021-05-10 15:44:54 +0200

    Minor changes

--- src/countOnes.py
+++ src/countOnes.py
@@ -2,7 +2,7 @@ import numpy as np
 import scipy.stats as sc
 import matplotlib.pyplot as plt
 
-BINARY_OUTPUT = "binaryout.bin"
+BINARY_OUTPUT = "random.bin"
 
 
 def countOnes():
@@ -16,7 +16,7 @@ def countOnes():
             bins.append(bit)
     for i in range(0, 2048000):
         counter = 0
-        value = bins[i:i+8]
+        value = bins[i:i + 8]
         for bit in value:
             counter += 1 if bit == '1' else 0
         if counter <= 2:
@@ -30,9 +30,13 @@ def countOnes():
         elif counter >= 6:
             letters.append('E')
 
-    words4, counts4 = joinToWord(letters, 4)
-    for word, count in zip(words4, counts4):
-        print(f"{word}: {count}")
+    Q4, counts4 = chiCalc(4, letters)
+    Q5, counts5 = chiCalc(5, letters)
+    stat4, chi4 = sc.chisquare(counts4)
+    stat5, chi5 = sc.chisquare(counts5)
+    print(stat4, chi4, stat5, chi5)
+    print(f"ChiSquare result= {Q5 - Q4}, Q5= {Q5}, Q4= {Q4}")
+    print(f"Oryg. ChiSquare result= {stat5 - stat4}, Q5= {stat5}, Q4= {stat4}")
 
 
 def joinToWord(letters: list, word_length: int):
@@ -42,4 +46,14 @@ def joinToWord(letters: list, word_length: int):
     return np.unique(words, return_counts=True)
 
 
-countOnes()
+def chiCalc(word_length: int, letters: list):
+    _, counts = joinToWord(letters, word_length)
+    expected = 256000 / word_length ** 5
+    result = 0
+    for val in counts:
+        result += ((val - expected) ** 2) / expected
+    return result, counts
+
+
+if __name__ == "__main__":
+    countOnes()
