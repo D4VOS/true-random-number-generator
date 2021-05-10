331dc95b4923987a787051c8d97fef368abf95be
D4VOS <dawid.bryla@student.put.poznan.pl>
2021-05-10 14:57:57 +0200

    Minor changes

--- src/countOnes.py
+++ src/countOnes.py
@@ -1,22 +1,24 @@
 import numpy as np
+import scipy.stats as sc
+import matplotlib.pyplot as plt
 
 BINARY_OUTPUT = "binaryout.bin"
 
 
 def countOnes():
     flile = open(BINARY_OUTPUT, 'r')
-    file_array = np.fromfile(flile, dtype=np.uint8)
-    bin_values = list()
+    file_array = np.fromfile(flile, dtype=np.uint32)
+    bins = list()
     letters = list()
     for item in file_array:
-        bin_values.append(np.binary_repr(item, 8))
-
-    for i in range(0, 100000):
+        value = np.binary_repr(item)
+        for bit in value:
+            bins.append(bit)
+    for i in range(0, 2048000):
         counter = 0
-        value = bin_values[i]
-        for j in range(0, 8):
-            if value[j] == '1':
-                counter += 1
+        value = bins[i:i+8]
+        for bit in value:
+            counter += 1 if bit == '1' else 0
         if counter <= 2:
             letters.append('A')
         elif counter == 3:
@@ -28,9 +30,16 @@ def countOnes():
         elif counter >= 6:
             letters.append('E')
 
+    words4, counts4 = joinToWord(letters, 4)
+    for word, count in zip(words4, counts4):
+        print(f"{word}: {count}")
+
 
 def joinToWord(letters: list, word_length: int):
     words = list()
-    for letter in range(0, 20000):
+    for letter in range(0, 256000):
         words.append("".join(letters[letter * word_length:letter * word_length + word_length]))
     return np.unique(words, return_counts=True)
+
+
+countOnes()
